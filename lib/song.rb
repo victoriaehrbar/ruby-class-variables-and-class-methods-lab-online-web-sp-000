54570af640a69995b1fe6b24041b42d2e91d97bd
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-03 03:40:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,16 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
   end
+  genre_count
+end
+end
 
 end
