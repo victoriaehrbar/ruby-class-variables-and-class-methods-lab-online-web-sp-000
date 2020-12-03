4196bc6b6ee2733bd55a1211bd1f33b247087942
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-03 03:34:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,12 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
